75d979f9dfe4cd5b6ce370a038b9f707d2dc5300
ArunDrioder <arunprasadh.s@gmail.com>
2025-05-28 08:37:19 +0530

    Added the test Case to compare the WiFi Setting alert title

--- src/test/java/AppiumBasics/AppiumBasics.java
+++ src/test/java/AppiumBasics/AppiumBasics.java
@@ -6,6 +6,7 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -24,6 +25,8 @@ public class AppiumBasics extends BaseTest
         driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
         driver.findElement(By.id("android:id/checkbox")).click();
         driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+        String alertTitleText = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitleText,"WiFi settings");
         driver.findElement(By.id("android:id/edit")).sendKeys("Airtel_Arun_5GHz");
         //driver.findElement(By.id("android:id/button1")).click();
         driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
